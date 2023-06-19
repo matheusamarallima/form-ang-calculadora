d9504bfc8c27ccad7a6a70eabfb0f6c914e13a85
Mathe <matheusamrl@gmail.com>
2023-06-19 17:56:24 -0300

    final version

--- src/app/calculadora/componentes/calculadora.component.spec.ts
+++ src/app/calculadora/componentes/calculadora.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalculadoraComponent } from './calculadora.component';
+import { CalculadoraService } from '../services';
+
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
@@ -8,7 +10,10 @@ describe('CalculadoraComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CalculadoraComponent]
+      declarations: [CalculadoraComponent],
+      providers: [
+        CalculadoraService
+      ]
     });
     fixture = TestBed.createComponent(CalculadoraComponent);
     component = fixture.componentInstance;
--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
 
@@ -13,4 +13,25 @@ describe('CalculadoraService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  
+  it('deve garantir que 1 + 4 = 5',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+    let soma = service.calcular(1, 4, CalculadoraService.SOMA);
+    expect(soma).toEqual(5);
+  }))
+  it('deve garantir que 1 - 4 = -3',
+  inject([CalculadoraService], (service: CalculadoraService)=>{
+    let subtra = service.calcular(1, 4, CalculadoraService.SUBS)
+    expect(subtra).toEqual(-3);
+  }))
+  it('deve garantir que 1 * 5 = 5', 
+  inject([CalculadoraService], (service: CalculadoraService)=> {
+    let mult = service.calcular(1, 5, CalculadoraService.MULTI)
+    expect(mult).toEqual(5);
+  }))
+  it('deve garantir que 4 / 2 = 2', 
+  inject([CalculadoraService], (service: CalculadoraService)=> {
+    let div = service.calcular(4, 2, CalculadoraService.DIVISAO)
+    expect(div).toEqual(2);
+  }))
 });
